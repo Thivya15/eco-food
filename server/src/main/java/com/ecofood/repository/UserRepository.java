fff97fc274caf055f265d04d21fdfc1ee6582388
Thivya15 <thivyamahendran15@gmail.com>
2025-09-15 08:27:48 +0530

    UserRepository commited

---
+++ server/src/main/java/com/ecofood/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ecofood.repository;
+
+import com.ecofood.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String username);
+}
